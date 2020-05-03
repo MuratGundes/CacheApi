59e0d344a9d5998d4e8bc7c63a274b8ec3f18d3e
murat.gundes <murat.gundes@hotmail.com>
2020-05-03 17:32:23 +0200

    schema registered.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+require('./api/Model/CacheSchema');
 const connection = mongoose.connection;
 
 const app = express();
@@ -9,7 +10,7 @@ const router = require('./api/Routers/CacheRouter');
 const config = require('./api/config.json');
 var __dirname = '/';
 
-mongoose.connect(config.database.host + config.database.name, { useUnifiedTopology: true, useNewUrlParser: true });
+mongoose.connect(config.database.host + config.database.name, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
 
 connection.once("open", function() {
     console.log("MongoDB database connection established successfully.");
